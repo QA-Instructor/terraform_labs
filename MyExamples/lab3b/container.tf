aa8898076d5db9ae587b538502ca84e6c9f0b6cd
Victoria Lloyd <qainstructorsky@gmail.com>
2024-11-27 12:53:59 +0000

    Corrected sa name attribute

--- MyExamples/lab3b/container.tf
+++ MyExamples/lab3b/container.tf
@@ -1,5 +1,5 @@
 resource "azurerm_storage_container" "lab3b-cont" {
   name                  = "lab3b-cont"
-  storage_account_name    = azurerm_storage_account.lab3bsa
+  storage_account_name    = azurerm_storage_account.lab3bsa.name
   container_access_type = "blob"
 }
