6a4cce8f8d7d2416c9e28ed7ecf37892f170548f
Victoria Lloyd <QAInstructorSky@gmail.com>
2024-11-27 12:33:39 +0000

    Lab 3b before challenge

---
+++ MyExamples/lab3b/container.tf
@@ -0,0 +1,5 @@
+resource "azurerm_storage_container" "lab3b-cont" {
+  name                  = "lab3b-cont"
+  storage_account_name    = azurerm_storage_account.lab3bsa
+  container_access_type = "blob"
+}
---
+++ MyExamples/lab3b/variable.tf
@@ -0,0 +1,5 @@
+variable "sa_name" {
+  description = "storageAccount for lab 3b"
+  type        = string
+  default     = "vl20240111"
+}
